0f77ef465621b46c7e45199939fe64a71f73b529
Apollopauljnr <apollopauljnr@yahoo.com>
2022-03-11 14:05:03 -0800

    commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "programming is like
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("size of char: %ld byte(s)\n", sizeof(c));
+	return (0);
+}
