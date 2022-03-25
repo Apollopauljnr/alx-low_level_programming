825bdb3da1c6a53f98c88e23b523f4c3cce142e0
Apollopauljnr <apollopauljnr@yahoo.com>
2022-03-25 13:13:00 -0700

    commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+/**
+ * -strncat - function that concatenates two strings
+ *  @dest: parmater for dest to append to
+ *  @src: parameter for src
+ *  @n: parameter for n
+ *  Return: Always 0.
+ */
 
-int  main(void)
-{
-	puts("\"iwhudfguujdbjdbjbjbjb");
-	return(0);
+	
+char *_strncat(char *dest, char *src, int n)
+{	
+	int a = 0;
+	int b = 0;
+
+	while (dest[a] != '\0')
+		a++;
+	while (src[b] != '\0' && b < n)
+	{
+		dest[a] = src[b];
+		b++;
+		a++;
+	}
+
+	return (dest);
 }
-
