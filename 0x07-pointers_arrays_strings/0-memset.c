b01879c6d90d65674d65e7aeb3be181c291ebfc7
Apollopauljnr <apollopauljnr@yahoo.com>
2022-03-28 17:34:31 -0700

    commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 /**
- * _memset - check the code for Holberton School students.
- * @s: parameter for the array
- * @b: parameter for the values in the array
- * @n: parameter for the number of times to print the new values
- * Return: Always 0.
+ * _memset - fills memory with a constant byte
+ * @s: pointer to memory area 's'
+ * @b: constant byte to fill
+ * @n: number of bytes to fill
+ * Return: pointer to memory area 's'
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int a = 0;
+	unsigned int i;
 
-	while (a < n)
-	{
-		s[a] = b;
-		a++;
-	}
-
+	for (i = 0; i < n; i++)
+		s[i] = b;
 	return (s);
 }
